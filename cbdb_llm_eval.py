65ba5691d4d7210a8f3b9339a6b8b926943dc689
Hongsu Wang <sudospace@gmail.com>
2024-12-17 22:59:57 -0500

    improve the addr belong q&a

--- cbdb_llm_eval.py
+++ cbdb_llm_eval.py
@@ -111,9 +111,11 @@ rows = c.fetchall()
 row_sample = random.sample(rows, min(len(rows), 10))
 
 for row in row_sample:
-    new_row = {'question': f"{row[1]}(c_personid={row[0]}的籍貫是否為{row[2]}？", 'answer': "是"}
-    new_row = {'question': f"{row[1]}(c_personid={row[0]}的籍貫是否為{row[3][1:]}？", 'answer': "否"}
-    df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+    new_rows = [
+        {'question': f"{row[1]}(c_personid={row[0]}的籍貫是否為{row[2]}？", 'answer': "是"},
+        {'question': f"{row[1]}(c_personid={row[0]}的籍貫是否為{row[3][1:]}？", 'answer': "否"}
+    ]
+    df = pd.concat([df, pd.DataFrame(new_rows)], ignore_index=True)
 
 df = df.sample(frac=1).reset_index(drop=True)
 print(df.head(10))
